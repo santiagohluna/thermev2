bd508b21f64280bc4b17f81f60319f48cb781c40
shluna <santiagohluna@gmail.com>
2023-01-07 18:35:51 -0300

    Se incluyó la subrutina que calcula la derivada temporal de la oblicuidad de la Tierra, ec. (A.6) de Farhat et al. (2022) Se agregaron algunos parámetros fundamentales, como los valores actuales del semieje de la órbita de la Luna, la longitud del día y frecuencia de precesión.

---
+++ src/oblicuidad.f90
@@ -0,0 +1,16 @@
+program oblicuidad
+    
+    use thermev2_subs
+
+    implicit none
+
+    real (kind=8) :: t
+    real (kind=8), parameter :: ti = 4.5d0
+    real (kind=8), parameter :: tf = 0.d0
+    real (kind=8), parameter :: dt = 0.01d0
+
+    t = ti
+
+
+    
+end program oblicuidad
