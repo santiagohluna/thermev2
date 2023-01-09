b8e4ec69c78791d4153140887bf0a5a8ac95eafe
shluna <santiagohluna@gmail.com>
2023-01-09 20:12:08 -0300

    Se creó el programa evaleps para probar el algoritmo que evalúa la oblicuidad calculada con la ec. (A.6) de Farhat et al. (2022).

---
+++ src/evaleps.f90
@@ -0,0 +1,30 @@
+program evaleps
+    
+    use thermev2_subs
+
+    implicit none
+    
+    integer :: l,kmax
+
+    real (kind=dp) :: x,y
+    real (kind=dp), parameter :: xin = 0.0001d0
+    real (kind=dp), parameter :: dx = 0.01d0
+
+    call leer_oblicuidad(kmax)
+
+    open(unit=11,file='../out/evaleps.out')
+
+    x = xin
+    l = 3
+
+    do while (x.le.tf)
+        y = oblicuidad(x,kmax,l)
+        write(11,*) x,y
+        x = x + dx
+    end do
+
+!100 format('El valor t =',1x,f4.2,1x,'Ga se encuentra entre los índices',1x,i2,1x,'y',1x,i2,'.')
+!    print *,' '
+!    print 100,x,j,j+1
+
+end program evaleps
