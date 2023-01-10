a7054d134594dbd6a76d141011eee30a3358fb77
shluna <santiagohluna@gmail.com>
2023-01-10 12:07:23 -0300

    Se realizaron modificaciones menores para asegurar la compatibilidad con las nuevas versiones de los subprogramas leer_oblicuidad y oblicuidad.

--- src/evaleps.f90
+++ src/evaleps.f90
@@ -3,24 +3,21 @@ program evaleps
     use thermev2_subs
 
     implicit none
-    
-    integer :: l,kmax
 
     real (kind=dp) :: x,y
     real (kind=dp), parameter :: xin = 0.0001d0
     real (kind=dp), parameter :: dx = 0.01d0
 
-    call leer_oblicuidad(kmax)
+    call leer_oblicuidad()
 
-    print *,'kmax = ',kmax
+    print *,'nmax = ',nmax
 
     open(unit=11,file='../out/evaleps.out')
 
     x = xin
-    l = 3
 
     do while (x.le.tf)
-        y = oblicuidad(x,kmax,l)
+        y = oblicuidad(x)
         write(11,*) x,y
         x = x + dx
     end do
--- src/oblicuidad.f90
+++ src/oblicuidad.f90
@@ -1,10 +1,9 @@
-program oblicuidad
+program prueba_oblicuidad
     
     use thermev2_subs
 
     implicit none
 
-    integer :: k
     real (kind=8) :: t,a1,a2,eps,da,p,lod1,lod2,thp,n,epsdeg
     real (kind=8) :: h1,h2,h3,h4,da2,da6
     real (kind=8), parameter :: ti = 0.d0
@@ -18,27 +17,26 @@ program oblicuidad
     
     open(unit=10,file='../out/oblicuidad.out')
 
-10  format(i2,1x,f4.2,1x,f7.4,1x,f15.6,1x,f4.2,1x,f4.2)
+10  format(f4.2,1x,f7.4,1x,f15.6,1x,f10.8,1x,f10.8)
 
     demid = 1
     ldem1 = demid.eq.1
 
-    k = 1
     t = ti
     eps = eps0
     epsdeg = eps*180.d0/pi
-    call modelo_dinamico(t,a1,n,lod1,thp)
     p = p0
 
     do while (t.le.tf+0.5d0)
-        write(10,10) k,t,epsdeg,p*(180.d0/pi)*3600.d0*aa,a1/a0,lod1/LOD0
         call modelo_dinamico(t,a1,n,lod1,thp)
         call modelo_dinamico(t+dt,a2,n,lod2,thp)
         da = a2 - a1
         da2 = 0.5d0*da
         da6 = da/6.d0
+!                    1   2                3              4       5
+        write(10,10) t,epsdeg,p*(180.d0/pi)*3600.d0*aa,a1/a0,lod1/LOD0
 !       -----------------------------------------------------------------------
-!       Integrtación de la ec. (A.6) de Farhat et al. (2020) usando el método
+!       Integración de la ec. (A.6) de Farhat et al. (2020) usando el método
 !       de Runge-Kutta de orden 4.
 !       -----------------------------------------------------------------------
         h1 = depsda(t,eps)
@@ -49,8 +47,7 @@ program oblicuidad
 !       -----------------------------------------------------------------------
         epsdeg = eps*180.d0/pi
         t = t + dt
-        k = k + 1
         p = 1.5d0*(GMs/(ae**3) + GMl/(a2**3))*Ed*thp*dcos(eps)/(thp0**2)
     end do
     
-end program oblicuidad
+end program prueba_oblicuidad
