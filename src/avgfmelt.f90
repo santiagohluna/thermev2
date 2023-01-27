5b8766d4875b72a16c30a48661e8e78ce94b147e
shluna <santiagohluna@gmail.com>
2023-01-27 15:44:50 -0300

    Se hicieron modificaciones menores para probar otra forma de calcular St

--- src/avgfmelt.f90
+++ src/avgfmelt.f90
@@ -5,7 +5,8 @@ program avg_fmelt
     implicit none
 
     real(kind=dp) :: avgTc,avgTm,Tcmb,Tubl,Tlbl,DTlbl,DTubl,dubl,dlbl, &
-                     St
+                     St,err,damfdT
+    real(kind=dp), parameter :: h = 10.d0
     
     open(unit=20,file='../out/avgfmelt.out',status='unknown')
 
@@ -16,20 +17,15 @@ program avg_fmelt
     Tcmb = avgTc/epsc
     Tubl = avgTm/epsm
 
-    call capas_limites(Tcmb,Tubl,Tlbl,DTubl,DTlbl,dubl,dlbl)
-
-!    print *,'T_lbl = ',Tlbl
-!    Tlbl = Tubl*(1.d0 + alfam*gum*(Rt-Rc-dubl-dlbl)/cm)
-!    print *,'T_lbl = ',Tlbl
-
     do while(avgTm.le.3000.d0)
         Tubl = avgTm/epsm
 
         call capas_limites(Tcmb,Tubl,Tlbl,DTubl,DTlbl,dubl,dlbl)
 
-        St = stefan(Tcmb,Tubl)
+        St = stefan(Tubl,dubl)
+        damfdT = dfridr(avgfmelt,Tcmb,Tubl,h,err)
 
-        write(20,*) avgTm,Tubl,avgfmelt(Tcmb,Tubl),St
+        write(20,*) avgTm,Tubl,avgfmelt(Tcmb,Tubl),(Lmelt/cm)*damfdT,St
 
         avgTm = avgTm + 10.d0
     end do
--- src/prueba_st.f90
+++ src/prueba_st.f90
@@ -4,35 +4,20 @@ program prueba_st
 
     implicit none
 
-    real(kind=dp) :: den,int,Tubl,avgTc,avgTm,Tcmb,Tlbl,DTubl,DTlbl, &
-                     dubl,dlbl,err,num,damfdT,r1,r2,Tcmb0,tsolm,tliqm,St
+    real(kind=dp) :: Tubl,avgTc,avgTm,Tcmb,Tlbl,DTubl,DTlbl, &
+                     dubl,dlbl,r1,r2,Tcmb0,St
     real(kind=dp), parameter :: km = 1.d-3
     real(kind=dp), parameter :: tprint = 0.d0
     real(kind=dp), parameter :: h = 10.d0
 
-!-----------------------------------------------------------------------
-!   Cálculo de los valores medios de las temperatura de liquidus y de
-!   solidus en el manto
-!-----------------------------------------------------------------------
-    den = (rt**3 - rc**3)
-    call qromb(tsolr2,rc,rt,int)
-    tsolm = 3.d0*int/den
-    call qromb(tliqr2,rc,rt,int)
-    tliqm = 3.d0*int/den
 !   --------------------------------------------------------------------
 !   calculo del numero de stefan
 !   --------------------------------------------------------------------
-10  format(a11,1x,f7.2)   
-    
-    St = Lmelt/(cm*(tliqm - tsolm))
-
-    print 10,'St =',St
-
-    print *,' '
+10  format(a11,1x,f7.2)
 
     Tsup = 300.d0
     avgTc = 6000.d0
-    avgTm = 2000.d0
+    avgTm = 3000.d0
 
     print 10,'Tc =',avgTc
     print 10,'Tm =',avgTm
@@ -71,9 +56,6 @@ program prueba_st
         print 10,'dlit =',(Rt - r2)*km
         print 10,'Rt - dubl =',(Rt - dubl)*km
         print *,' '
-        !damfdT = dfridr(avgfmelt,Tcmb,Tubl,h,err)
-
-        num = r2**3 - r1**3
 
         St = stefan(Tubl,dubl)
 
