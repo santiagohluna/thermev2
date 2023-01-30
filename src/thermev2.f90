376301a7f006aab2af763b88553b6aad67f4674b
shluna <santiagohluna@gmail.com>
2023-01-29 21:37:28 -0300

    Se realizaron cambios menores

--- src/thermev2.f90
+++ src/thermev2.f90
@@ -5,17 +5,19 @@ program thermev_db
 !-----------------------------------------------------------------------
     implicit none
 !-----------------------------------------------------------------------
-    integer, parameter :: nvar = 3
-    integer :: nbad,nok,l,m,p,q,ok
+    integer, parameter :: nvar = 2
+    integer :: nbad,nok,l,m,p,q,ok,demid,tidefl,radcfl,radmfl
     character cr
-    real (kind=dp) :: t,tprint,y(nvar),dydt(nvar),a,ric,&
+    real(kind=dp) :: t,tprint,y(nvar),dydt(nvar),a,ric,&
                       ur,urtot,dlit,qcmb,qconv,qmelt,qradm, & 
-                      qradc,tcmb,tubl,qtidal,tpot,Ra,den,int, &
-                      Tlbl,DTubl,DTlbl,dubl,dlbl,chi
-    real (kind=dp), parameter :: tol = 1.d-6
-    real (kind=dp), parameter :: dt = 1.d-3
-    real (kind=dp), parameter :: tera = 1.d-12
-    real (kind=dp), parameter :: km = 1.d-3
+                      qradc,tcmb,tubl,qtidal,tpot,Ra,St, &
+                      Tlbl,DTubl,DTlbl,dubl,dlbl,dtprint
+    real(kind=dp) :: tc0,tm0,DTcmb,DTlbl0
+    logical, dimension(2) :: linit
+    real(kind=dp), parameter :: tol = 1.d-6
+    real(kind=dp), parameter :: dt = 1.d-3
+    real(kind=dp), parameter :: tera = 1.d-12
+    real(kind=dp), parameter :: km = 1.d-3
 !-----------------------------------------------------------------------
 !   cálculos preliminares
 !-----------------------------------------------------------------------
@@ -28,15 +30,6 @@ program thermev_db
     ma = (af-ai)/4.5d0
     mlod = (lodf-lodi)/4.5d0
 !-----------------------------------------------------------------------
-!   Cálculo de los valores medios de las temperatura de liquidus y de
-!   solidus en el manto
-!-----------------------------------------------------------------------
-    den = (rt**3 - rc**3)
-    call qromb(tsolr2,rc,rt,int)
-    tsolm = 3.d0*int/den
-    call qromb(tliqr2,rc,rt,int)
-    tliqm = 3.d0*int/den
-!   --------------------------------------------------------------------
     cr = achar(13)
     print *,'... listo!'
 !-----------------------------------------------------------------------
@@ -51,7 +44,8 @@ program thermev_db
 !   lectura de archivo de entrada
 !-----------------------------------------------------------------------
     print *,'Leyendo archivo de entrada...'
-    call leer_entrada()
+    call leer_entrada(tc0,tm0,DTcmb,DTlbl0,linit,demid,tidefl, &
+    radcfl,radmfl,dtprint)
     print *,'... listo!'
     nterms = 0
     do l=2,lmax
@@ -85,7 +79,7 @@ program thermev_db
 !   creación de archivo de salida
 !-----------------------------------------------------------------------
     print *,'Creando archivo de salida...'
-    call crear_archivo_salida()
+    call crear_archivo_salida(demid,tidefl,radcfl,radmfl)
     print *,'... listo!'
 !-----------------------------------------------------------------------
 !   inicialización de las temperaturas medias del manto y del núcleo
@@ -95,12 +89,10 @@ program thermev_db
     tprint = 0.d0
     dtprint = dtprint/1000.d0
       y(1) = tc0/epsc
-      y(2) = 0.d0
-      y(3) = tm0/epsm
+      y(2) = tm0/epsm
     call dTdt(t,y,dydt)
     Tcmb = y(1)
-    Ric = dsqrt(y(2))
-    Tubl = y(3)
+    Tubl = y(2)
     Ur = printout(1)
     Urtot = printout(2)
      Qcmb = printout(3)
@@ -110,12 +102,11 @@ program thermev_db
     Qradc = printout(7)
    Qtidal = printout(8)
        St = printout(9)
-       Ra = printout(10)
-      chi = printout(11)
-    call conveccion(Tcmb,Tubl,Tlbl,DTubl,DTlbl,dubl,dlbl)
+      Ric = printout(10)
+       Ra = printout(11)
+    call capas_limites(Tcmb,Tubl,Tlbl,DTubl,DTlbl,dubl,dlbl)
     print *,'dTcmb/dt = ',dydt(1)
-    print *,'dRic2/dt = ',dydt(2)
-    print *,'dTm/dt = ',dydt(3)
+    print *,'dTm/dt = ',dydt(2)
     dlit = (rt - Rlit(tubl,dubl))*km
     tpot = tubl*dexp(-gum*alfam*dubl/cm)
     print *,'... listo!'
@@ -127,6 +118,7 @@ program thermev_db
     print 13,'  T_cmb =',Tcmb,'K'
     print 13,'    T_m =',Tm0,'K'
     print 13,'  T_ubl =',Tubl,'K'
+    print 13,'  T_lbl =',Tlbl,'K'
     print 13,'  T_pot = ',Tpot,'K'
     print 13,'   R_ic =',ric*km,'km'
     print 13,'    Lit =',dlit,'km'
@@ -148,11 +140,10 @@ program thermev_db
 !       ----------------------------------------------------------------
         if (t.ge.tprint) then
             call imprimir_perfil(tprint,Tcmb,Tubl)
-            call imprimir_Tliqc(tprint,chi)
             tprint = tprint + dtprint
         end if
 !       ----------------------------------------------------------------
-        call conveccion(Tcmb,Tubl,Tlbl,DTubl,DTlbl,dubl,dlbl)
+        call capas_limites(Tcmb,Tubl,Tlbl,DTubl,DTlbl,dubl,dlbl)
 !       ----------------------------------------------------------------
 !       calculo del espesor de la litosfera
 !       ----------------------------------------------------------------
@@ -171,8 +162,8 @@ program thermev_db
          Qradc = printout(7)
         Qtidal = printout(8)
             St = printout(9)
-            Ra = printout(10)
-           chi = printout(11)
+           Ric = printout(10)
+            Ra = printout(11)
 !       ----------------------------------------------------------------
 !       escritura de resultados en el archivo de salida
 !       ----------------------------------------------------------------
@@ -182,13 +173,12 @@ program thermev_db
                     dlit,Ric*km,dubl*km,dlbl*km, &
         !                11       12         13         14
                     qcmb*tera,qconv*tera,qmelt*tera,qradm*tera, &
-        !               15          16        17      18 19  
-                    qradc*tera,qtidal*tera,visc(Tubl),Ra,chi
+        !               15          16        17      18 19
+                    qradc*tera,qtidal*tera,visc(Tubl),Ra,St
 !       ----------------------------------------------------------------
         call odeint(y,nvar,t,t+dt,tol,dt,0.d0,nok,nbad,dTdt,bsstep)    
         Tcmb = y(1)
-        Ric = dsqrt(y(2))
-        Tubl = y(3)
+        Tubl = y(2)
 !       ----------------------------------------------------------------
         write(*,12,advance='no') cr,t*100.d0/tf,'%'
         t = t + dt
@@ -204,8 +194,9 @@ program thermev_db
     print 13,'  T_cmb = ',Tcmb,'K'
     print 13,'    T_m = ',epsm*Tubl,'K'
     print 13,'  T_ubl = ',Tubl,'K'
+    print 13,'  T_lbl =',Tlbl,'K'
     print 13,'  T_pot = ',Tpot,'K'
-    print 13,'   R_ic = ',ric/1000.d0,'km'
+    print 13,'   R_ic = ',Ric/1000.d0,'km'
     print 13,'    Lit = ',dlit,'km'
     print 13,'  Q_cmb = ',qcmb*tera,'TW'
     print 13,' Q_conv = ',qconv*tera,'TW'
@@ -220,7 +211,6 @@ program thermev_db
 !   --------------------------------------------------------------------
     print *,'Imprimiendo perfil de temperatura final...'
     call imprimir_perfil(tprint,Tcmb,Tubl)
-    call imprimir_Tliqc(tprint,chi)
     print *,'... listo!'
 !   --------------------------------------------------------------------
     print *,' '
